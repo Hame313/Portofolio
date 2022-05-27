f3e637f2f25f5ff9202aeb515541c921a926e701
Hame313 <beganhame@gmail.com>
2022-05-27 11:12:16 +0200

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const burger=document.querySelector('.menu-btn__burger');
-const menu=document.querySelector('.menu-nav');
-let click=0;
-burger.addEventListener('click',()=>{  
-   menu.classList.toggle("show")
-    
-})
+
+  
